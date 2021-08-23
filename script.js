cc578db819e59d0debb6d06b3899c3761809b531
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-23 09:48:06 -0700

    refactored the code

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const nav2 = document.querySelector("#nav-2");
 const nav3 = document.querySelector("#nav-3");
 const nav4 = document.querySelector("#nav-4");
 const nav5 = document.querySelector("#nav-5");
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control NAvigation Animation
+const navAnimation = (direction1, direction2) => {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}, slide-${direction2}-${i + 1}`
+    );
+  });
+};
 
 // Nav toggler
 const toggleNav = () => {
@@ -17,38 +27,18 @@ const toggleNav = () => {
     overlay.classList.remove("overlay-slide-left");
     overlay.classList.add("overlay-slide-right");
     // Animate In the Nav Items
-    nav1.classList.remove("slide-out-1");
-    nav1.classList.add("slide-in-1");
-    nav2.classList.remove("slide-out-2");
-    nav2.classList.add("slide-in-2");
-    nav3.classList.remove("slide-out-3");
-    nav3.classList.add("slide-in-3");
-    nav4.classList.remove("slide-out-4");
-    nav4.classList.add("slide-in-4");
-    nav5.classList.remove("slide-out-5");
-    nav5.classList.add("slide-in-5");
+    navAnimation("out", "in");
   } else {
     // Animate Out the Overlay
     overlay.classList.remove("overlay-slide-right");
     overlay.classList.add("overlay-slide-left");
     // Animate out the nav items
-    nav1.classList.remove("slide-in-1");
-    nav1.classList.add("slide-out-1");
-    nav2.classList.remove("slide-in-2");
-    nav2.classList.add("slide-out-2");
-    nav3.classList.remove("slide-in-3");
-    nav3.classList.add("slide-out-3");
-    nav4.classList.remove("slide-in-4");
-    nav4.classList.add("slide-out-4");
-    nav5.classList.remove("slide-in-5");
-    nav5.classList.add("slide-out-5");
+    navAnimation("in", "out");
   }
 };
 
 // Add event listeners
 menuBars.addEventListener("click", toggleNav);
-nav1.addEventListener("click", toggleNav);
-nav2.addEventListener("click", toggleNav);
-nav3.addEventListener("click", toggleNav);
-nav4.addEventListener("click", toggleNav);
-nav5.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
