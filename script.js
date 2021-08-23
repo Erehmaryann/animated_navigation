7ddd61eaad57431085cbbb290d800564b004f543
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-22 18:29:36 -0700

    selected the 3rd nav item needed in the Js file

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const menuBars = document.querySelector("#menu-bars");
 const overlay = document.querySelector("#overlay");
 const nav1 = document.querySelector("#nav1");
 const nav2 = document.querySelector("#nav2");
+const nav3 = document.querySelector("#nav3");
