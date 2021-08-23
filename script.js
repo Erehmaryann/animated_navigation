06034c5120145ec9828ed7e409f35cd8e8065a8d
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-22 18:52:33 -0700

    declared the toggle nav function

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const nav4 = document.querySelector("#nav4");
 const nav5 = document.querySelector("#nav5");
 
 // Nav toggler
-const toggleNav = () => {};
+const toggleNav = () => {
+  // Toggle: Menu Bars Open/CLosed
+};
 
 // Add event listeners
 menuBars.addEventListener("click", toggleNav);
@@ -15,3 +17,4 @@ nav1.addEventListener("click", toggleNav);
 nav2.addEventListener("click", toggleNav);
 nav3.addEventListener("click", toggleNav);
 nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
