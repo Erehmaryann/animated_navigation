4b585fee8d9c1e05f96612b0ae8efdb7f9bc75a3
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-22 18:34:29 -0700

    selected the 4th nav item needed in the Js file

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const overlay = document.querySelector("#overlay");
 const nav1 = document.querySelector("#nav1");
 const nav2 = document.querySelector("#nav2");
 const nav3 = document.querySelector("#nav3");
+const nav4 = document.querySelector("#nav4");
